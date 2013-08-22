1744453dc6bc11e20b65300a43b8f616903fdaed
piatra <andrei.br92@gmail.com>
2013-08-22 13:07:10 +0300

    update to Topdoc 0.2.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,9 +86,13 @@ module.exports = function(grunt) {
             usageguides: {
                 options: {
                     source: 'css',
-                    destination: './',
-                    template: '<%= pkg.topdoc.template %>',
-                    templateData: '<%= pkg.topdoc.templateData %>'
+                    destination: "test/component",
+                    template: "node_modules/topdoc-theme/",
+                    templateData: {
+                      "title": "Topcoat",
+                      "subtitle": "CSS for clean and fast web apps",
+                      "homeURL": "http://topcoat.io"
+                    }
                 }
             }
         },
